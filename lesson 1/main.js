d08a57b93f4819df50094ac06e710aa6b2b1d408
prototypeemirlan <booker_fred@mail.ru>
2022-04-09 19:37:14 +0600

    Hello

---
+++ lesson 1/main.js
@@ -0,0 +1,48 @@
+// const a= "a";
+// const b= 20;
+// const bool= true;
+// const bool2= false;
+//
+// console.log(bool)
+
+//
+// let c=  30
+// console.log(c)
+//
+// c = "Dffdfd"
+//
+//
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// const obj= {
+//     key: "value",
+//     firs_name: 'Jack',
+//     data: {
+//         firs_name: 'Barbaro',
+//         last_name: 'John',
+//     }
+ //object
+// console.log(obj)
+// console.log(obj.firs_name)
+// console.log(obj.key)
+// console.log(obj.data.firs_name)
+// console.log(obj.data.last_name)
+
+// const arr= ["adf", 234, true, false, "afds"]
+// console.log(arr[0], arr[1])
+//
+// console.log(arr)
+
+// const name= prompt("как вас зовут")
+// console.log()
+// const name2= alert("ошибка  ")
+const first_name= prompt("первое слово")
+const last_name= prompt("второе слово")
+if (first_name > last_name) {
+    console.log(first_name + ">" + last_name)
+} else if (first_name < last_name) {
+    console.log(first_name + ">" + last_name)
+} else {
+    alert(first_name + "=" + last_name)
+}
